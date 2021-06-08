3684bb10e25ba15f34cf94966431f25317526e9f
shaqran_bhat <bhatshakran@gmail.com>
2021-06-08 19:33:16 +0530

    Core class modified , set current controller according to the url otherwise set it to default currentcontroller

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -11,10 +11,34 @@
         protected $params = [];
 
         public function __construct() {
-            $this->getUrl();
+            $url = $this->getUrl();
+
+            // Look in controllers for first value
+            if(file_exists('../app/controllers/' .ucwords($url[0]) .'.php')){
+                // If exists set it as the current controller 
+                $this->currentController = ucwords($url[0]);
+
+                // Unset the  0 Index
+                unset($url[0]);
+
+                
+            }
+
+            // Require the controller 
+            require_once '../app/controllers/' . $this->currentController . '.php';
+
+
+            // Once we get that controller instantiate that controllers class
+            $this->currentController = new $this->currentController;
         }
 
         public function getUrl(){
-            echo $_GET['url'];
+            if(isset($_GET['url'])){
+                $url = rtrim($_GET['url'], '/');
+                $url = filter_var($url, FILTER_SANITIZE_URL);
+                $url = explode('/', $url);
+                return $url;
+            }
+           
         }
     }
